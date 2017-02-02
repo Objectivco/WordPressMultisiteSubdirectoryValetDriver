351e00f5f4db078e9c83aee276ef3d15d1356a81
Clifton Griffin <clifgriffin@gmail.com>
2017-02-02 09:40:59 -0500

    Make sure wp-config.php exists so that we don’t get PHP warnings.

--- WordPressMultisiteSubdirectoryValetDriver.php
+++ WordPressMultisiteSubdirectoryValetDriver.php
@@ -11,10 +11,10 @@ class WordPressMultisiteSubdirectoryValetDriver extends BasicValetDriver
      */
     public function serves($sitePath, $siteName, $uri)
     {
-    	// Look for MULTISITE in wp-config.php. It should be there for multisite installs. 
-    	return strpos( file_get_contents($sitePath . '/wp-config.php'), 'MULTISITE') !== false; 
+    	// Look for MULTISITE in wp-config.php. It should be there for multisite installs.
+    	return file_exists($sitePath . '/wp-config.php') && strpos( file_get_contents($sitePath . '/wp-config.php'), 'MULTISITE') !== false; 
     }
-    
+
     /**
      * Get the fully resolved path to the application's front controller.
      *
@@ -27,36 +27,36 @@ class WordPressMultisiteSubdirectoryValetDriver extends BasicValetDriver
     {
         $_SERVER['PHP_SELF']    = $uri;
         $_SERVER['SERVER_ADDR'] = '127.0.0.1';
-        $_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'];              
-        
-        // If URI contains one of the main WordPress directories, and it's not a request for the Network Admin, 
+        $_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'];
+
+        // If URI contains one of the main WordPress directories, and it's not a request for the Network Admin,
         // drop the subdirectory segment before routing the request
         if ( ( stripos($uri, 'wp-admin') !== false || stripos($uri, 'wp-content') !== false || stripos($uri, 'wp-includes') !== false ) && stripos($uri, 'wp-admin/network') === false ) {
 			$uri = substr($uri, stripos($uri, '/wp-') );
        	}
-        
+
         return parent::frontControllerPath(
             $sitePath, $siteName, $this->forceTrailingSlash($uri)
         );
     }
-    
+
     public function isStaticFile($sitePath, $siteName, $uri)
     {
-    	// If the URI contains one of the main WordPress directories and it doesn't end with a slash, 
+    	// If the URI contains one of the main WordPress directories and it doesn't end with a slash,
     	// drop the subdirectory from the URI and check if the file exists. If it does, return the new uri.
-        if ( stripos($uri, 'wp-admin') !== false || stripos($uri, 'wp-content') !== false || stripos($uri, 'wp-includes') !== false ) { 
-        	if ( substr($uri, -1, 1) == "/" ) return false; 
-        	      		
+        if ( stripos($uri, 'wp-admin') !== false || stripos($uri, 'wp-content') !== false || stripos($uri, 'wp-includes') !== false ) {
+        	if ( substr($uri, -1, 1) == "/" ) return false;
+
        		$new_uri = substr($uri, stripos($uri, '/wp-') );
-       		
+
        		if ( file_exists( $sitePath . $new_uri ) ) {
        			return $sitePath . $new_uri;
        		}
        	}
-       	
+
         return parent::isStaticFile( $sitePath, $siteName, $uri );
     }
-    
+
     /**
      * Redirect to uri with trailing slash.
      *
